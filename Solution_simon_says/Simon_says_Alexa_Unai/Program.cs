e1ba9dbcaf4d688ef395def821b65678a321c7c8
Unikragolas222 <unai_celaya@euneiz.com>
2024-12-19 09:01:17 +0100

    Added algorithm to the project

--- Solution_simon_says/Simon_says_Alexa_Unai/Program.cs
+++ Solution_simon_says/Simon_says_Alexa_Unai/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Inicializar secuencia y funcion random
+// Juego iniciado (Jugando)
+    // Crear variable puntos
+    // Crear pantalla de inicio
+    // Animación de juego
+    // Crear bucle de secuencia de color para jugar
+        // Mostrar color
+        // Pedir una respuesta al usuario
+        // Añadir puntos en caso de acertar
+        // Crear mensaje de error
+        // Crear bucle para volver a jugar o no
+    // Mostrar un color en consola
+    // Comprobar la respuesta del usuario
